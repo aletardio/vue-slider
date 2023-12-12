41893aa64c1227a3a33ab2abe006fd8ce4fa2658
aletardio <ale.tardio99@gmail.com>
2023-12-12 15:11:25 +0100

    added methods for nextImg and prevImg

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ const { createApp } = Vue;
 createApp ({
     data() {
         return {
+            activeImage: 0,
             slides: [
                         {
-                        image: 'img/01.webp',
+                            image: 'img/01.webp',
                             title: 'Marvel\'s Spiderman Miles Morale',
                             text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
                         }, {
@@ -28,4 +29,22 @@ createApp ({
                     ]
                 }
             },
+    methods: {
+        nextImg(){
+            if(this.activeImage === this.slides.image.length - 1){
+                this.activeImage = 0;
+            }
+            else {
+                this.activeImage++;
+            }
+        },
+        prevImg(){
+            if(this.activeImage == 0){
+                this.activeImage = this.slides.image.length - 1;
+            }
+            else {
+                this.activeImage--;
+            }
+        },
+    },
 }).mount('#app');
